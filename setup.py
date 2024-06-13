6f1cd125f1c1b6503d9c86e7baa9a4c3d8d05cd4
P-Sai-Sushma <sushma.pathange@bhrish.com>
2024-06-13 18:06:42 +0530

    new package

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        # Run the post-install script
+        subprocess.check_call([sys.executable, 'post_install.py'])
 
 setup(
     name="FileUploadAPI",
@@ -13,7 +23,7 @@ setup(
     package_data={
         '': ['templates/*.html'],
     },
-    author=" Sushma",
+    author="Sushma",
     author_email="saisushma.patange@gmail.com",
     description="This API is used to accept file uploads from end users.",
     long_description=open('README.md').read(),
@@ -25,5 +35,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    
+    cmdclass={
+        'install': PostInstallCommand,
+    },
 )
