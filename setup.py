b1159319aea656eec9512409a7dd807e9050b855
P-Sai-Sushma <160228653+P-Sai-Sushma@users.noreply.github.com>
2024-06-17 10:52:54 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools.command.install import install
 import subprocess
 import sys
 
-#class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-   # def run(self):
-       # install.run(self)
-        # Run the post-install script
-        #subprocess.check_call([sys.executable, 'post_install.py'])
+# class PostInstallCommand(install):
+# """Post-installation for installation mode."""
+#    # def run(self):
+#        # install.run(self)
+#         # Run the post-install script
+#         #subprocess.check_call([sys.executable, 'post_install.py'])
 
 setup(
     name="FileUploadAPI",
