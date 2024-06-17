fc06705bd44a8bee148c4846f54e3cf692ef3072
P-Sai-Sushma <160228653+P-Sai-Sushma@users.noreply.github.com>
2024-06-17 11:23:58 +0530

    Updated Package

--- test/test_app.py
+++ test/test_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 import tempfile
 
-from fileuploadapi.app import app, UPLOADS_DIR  # Adjust this import according to your module structure
+from fileuploadapi.app import app, UPLOADS_DIR  
 
 @pytest.fixture
 def client():
@@ -23,19 +23,16 @@ def client():
     os.unlink(app.config['DATABASE'])
 
 def test_index(client):
-    """Test the index page."""
     rv = client.get('/')
     assert rv.status_code == 200
     assert b'Welcome' in rv.data
 
 def test_upload_page(client):
-    """Test the upload page."""
     rv = client.get('/upload')
     assert rv.status_code == 200
     assert b'Upload' in rv.data
 
 def test_upload_file_success(client):
-    """Test uploading a file successfully."""
     data = {
         'file': (tempfile.NamedTemporaryFile(delete=False, suffix='.txt'), 'test.txt')
     }
@@ -44,17 +41,14 @@ def test_upload_file_success(client):
     assert b'File uploaded successfully' in rv.data
     # Check if file is saved in the uploads directory
     assert (UPLOADS_DIR / 'test.txt').exists()
-    # Clean up
     os.remove(UPLOADS_DIR / 'test.txt')
 
 def test_upload_file_no_file(client):
-    """Test uploading without selecting a file."""
     rv = client.post('/uploadFile', data={}, content_type='multipart/form-data')
     assert rv.status_code == 400
     assert b'Please select a file' in rv.data
 
 def test_upload_file_empty_filename(client):
-    """Test uploading a file with an empty filename."""
     data = {
         'file': (tempfile.NamedTemporaryFile(delete=False, suffix=''), '')
     }
@@ -63,8 +57,7 @@ def test_upload_file_empty_filename(client):
     assert b'File not selected' in rv.data
 
 def test_upload_file_too_large(client):
-    """Test uploading a file that is too large."""
-    # Create a temporary large file
+    # Creating a temporary large file
     large_file = tempfile.NamedTemporaryFile(delete=False, suffix='.txt')
     large_file.write(b'a' * (1048577))  # Write 1MB + 1 byte
     large_file.seek(0)
@@ -79,7 +72,6 @@ def test_upload_file_too_large(client):
     assert b'File size is too big. Upload file smaller in size' in rv.data
 
 def test_internal_server_error(client, mocker):
-    """Test internal server error during file upload."""
     data = {
         'file': (tempfile.NamedTemporaryFile(delete=False, suffix='.txt'), 'test.txt')
     }
